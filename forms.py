120724b32873d346acb6441201dafe5182b9dbe5
preetham <preetham.mervin@gmail.com>
2020-10-12 12:15:02 +0100

    Made registration and login forms using the module Flask-WTF and then rendered it using the according HTML templates. Forms now are functional and appear on the website but do not submit as of yet. Currently working on using SQPAlchemy for user management.

--- forms.py
+++ forms.py
@@ -1,39 +1,33 @@
-from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf import FlaskForm
 from wtforms import (StringField,
                      SubmitField,
                      PasswordField,
                      DateField,
                      SelectField)
 from wtforms.validators import (DataRequired,
-                                Email,
                                 EqualTo)
+
 
 class RegisterForm(FlaskForm):
     """Sign up for a OCRTunes account."""
     first_name = StringField('First Name', [DataRequired()])
     last_name = StringField('Last Name', [DataRequired()])
-    # email = StringField('Email', [Email(message=('Not a valid email address.')), DataRequired()])
     username = StringField('Username', [DataRequired()])
-    password = PasswordField('Password', [
-        DataRequired(message="Please enter a password."),
-    ])
+    password = PasswordField('Password', [DataRequired(message="Please enter a password.")])
     confirmPassword = PasswordField('Repeat Password', [EqualTo(password, message='Passwords must match.')])
     genre = SelectField(
         'Genre', [DataRequired()],
-        choices=[('Pop', 'pop'), 
+        choices=[('Pop', 'pop'),
                 ('Hip-Hop', 'rap'),
-                ('Rock/Heavy Metal', 'rock'), 
+                ('Rock/Heavy Metal', 'rock'),
                 ('Jazz', 'jazz')])
     favourite_artist = StringField('Favourite Artist', [DataRequired()])
     birthday = DateField('Your Date of Birth')
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+    submit = SubmitField('Register')
 
 
 class LoginForm(FlaskForm):
     """Sign In."""
-    username = StringField('Name', [DataRequired()])
-    password = PasswordField('Password', [
-        DataRequired(message="Please enter a password."),
-    ])
+    username = StringField('Username', [DataRequired()])
+    password = PasswordField('Password', [DataRequired(message="Please enter a password.")])
     submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,34 +1,54 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from forms import RegisterForm, LoginForm
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'string'
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#
+# db = SQLAlchemy(app)
+#
+#
+# class users(db.Model):
+#     _id = db.Column(db.Integer, primary_key="True")
+#     first_name = db.Column(db.String(100))
+#     last_name = db.Column(db.String(100))
+#     username = db.Column(db.String(100))
+#     password = db.Column(db.String(100))
+#     genre = db.Column(db.String(100))
+#     favourite_artist = db.Column(db.String(100))
+#     birthday = db.Column(db.String(100))
+#
+#     def __init__(self, first_name, last_name, username, password, genre, favourite_artist, birthday):
+#         self.first_name = first_name
+#         self.last_name = last_name
 
 
 @app.route("/", methods=["GET", "POST"])
-def homepage():
-    return render_template("base.html")
+def home():
+    return render_template("home.html")
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login_form():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for("/listennow"))
+        return redirect(url_for("/playlists"))
     return render_template("login.html", form=form)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register_form():
-    form = RegisterForm()
+    form = RegisterForm(request.form)
     if form.validate_on_submit():
-        return redirect(url_for("/sucess"))
+        return redirect(url_for("/success"))
     return render_template("register.html", form=form)
 
 
 @app.route("/success", methods=["GET", "POST"])
 def success():
-    return render_template("success.html")
+    return redirect(url_for("/success"))
 
 
 @app.route("/playlists", methods=["GET", "POST"])
@@ -37,4 +57,5 @@ def playlist():
 
 
 if __name__ == "__main__":
+    # db.create_all()
     app.run()
